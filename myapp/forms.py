b90dcf8a06f3a1b20a4c0f14080bc7cf5b6b8f67
pradeephuded37 <pradeephuded37@gmail.com>
2020-08-28 14:09:25 +0530

    second commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -14,15 +14,14 @@ class SampleForm(forms.Form):
     validators=[validate_name])
 
     last_name=forms.CharField(max_length=200,required=True,label="last name :")
-
+    
     email=forms.EmailField(max_length=200,required=True,label="email :",
     validators=[validators.MinLengthValidator(10)])
-
-    confirm_email=forms.EmailField(max_length=100,required=True,label="confirm email :",)
     
+    confirm_email=forms.EmailField(max_length=100,required=True,label="confirm email :",)
     pwd=forms.CharField(max_length=100,required=True,label="password :",widget=forms.PasswordInput(attrs={'placeholder':"password"}))
-
     pic=forms.ImageField(max_length=200,required=True,label="pic :")
+    botcacher=forms.CharField(max_length=10,required=False,widget=forms.HiddenInput)
 
     def clean(self,*args,**kwargs):
         cleaned_data=super().clean()#getting all the data that is filled in the form
@@ -32,4 +31,19 @@ class SampleForm(forms.Form):
             return cleaned_data
         self.add_error('confirm_email',"Both the emails are not same")#this method will decide to which field we need to show the error
         #self.add_error("fieldname",error message)
+
+    def clean_name(self):
+        name=self.cleaned_data.get("name") #is used to get the name that we have filled in the form
+        for i in name:
+            if not('a'<=i<='z' or 'A'<=i<='Z'):
+                raise ValidationError("Name must contain only alphabets")
+        return name
+
+    def clean_botcacher(self):
+        data=self.cleaned_data.get("botcacher")
+        if len(data)==0:
+            return data
+        self.add_error("name","you are not an human")
+    
+    
 
